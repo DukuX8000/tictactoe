d0544d3ba502391c3b25d1d2456e7bfb8e510633
DukuX8000 <cunnam2002@gmail.com>
2023-12-20 16:34:02 +0700

    Initial commit

---
+++ UI.h
@@ -0,0 +1,14 @@
+#ifndef __UI_H__
+#define __UI_H__
+
+void ingame(char broad[]);
+void info(char name[]);
+void gameMenu(char name[]);
+void loginMenu();
+void rankNav();
+void score();
+
+void normalmenu(char name[]);
+void rankmenu(char name[]);
+
+#endif
